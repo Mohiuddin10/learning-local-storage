57721152cfbab778898003922385d4c921cc62f3
Mohiuddin10 <sj.ctg6737@gmail.com>
2023-10-17 06:05:08 +0600

    LS load and show

--- local.js
+++ local.js
@@ -1,7 +1,8 @@
 const addProduct = () => {
     const product = getProduct('ProductName');
     const quantity = getProduct('ProductQuantity');
-    localStorage.setItem(product, quantity);
+    showProduct(product, quantity);
+    saveProductToLS(product, quantity);
 }
 
 const getProduct = (id) => {
@@ -11,3 +12,37 @@ const getProduct = (id) => {
     return product;
 }
 
+const showProduct = (product, quantity) => {
+    const ul = document.getElementById('product-list');
+    const li = document.createElement('li');
+    li.innerText = `${product} : ${quantity}`
+    ul.appendChild(li);
+}
+
+const getShoppingCartLS = () => {
+    const shoppingCart = localStorage.getItem('cart');
+    let cart = {};
+    if(shoppingCart){
+        cart = JSON.parse(shoppingCart);
+    }
+    return cart;
+}
+
+const saveProductToLS = (product, quantity) => {
+    const cart = getShoppingCartLS();
+    cart[product] = quantity;
+    const cartStringfied = JSON.stringify(cart);
+    localStorage.setItem('cart', cartStringfied);
+}
+
+const displayProductFromLS = () => {
+    const savedCart = getShoppingCartLS();
+    console.log(savedCart);
+    for (const product in savedCart) {
+        const quantity = savedCart[product];
+        console.log(product , quantity);
+        showProduct(product, quantity);
+    }
+
+}
+displayProductFromLS();
